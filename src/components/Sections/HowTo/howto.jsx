67243661bd2bfa7e93049643ff14c84d996ba74d
4ebula <4ebula@gmail.com>
2021-06-22 12:40:27 +0300

    fix: resolve errors in howto

--- src/components/Sections/HowTo/howto.jsx
+++ src/components/Sections/HowTo/howto.jsx
@@ -5,13 +5,13 @@ require('./howto.scss');
 export class Howto extends React.Component {
   render() {
     return (
-      <section class="howto" id="howto">
-        <div class="wrapper">
-          <div class="wrapper-inner">
-            <div class="howto-image">
+      <section className="howto" id="howto">
+        <div className="wrapper">
+          <div className="wrapper-inner">
+            <div className="howto-image">
               <img src={require("../../../assets/images/lion.jpg")} alt="Lion"/>
             </div>
-            <div class="howto-info">
+            <div className="howto-info">
               <h2>How Zoo online works</h2>
               <h3>Our service will give you an unforgettable experience of observing
                 and interacting with animals remotely
@@ -20,7 +20,7 @@ export class Howto extends React.Component {
                 of different animals in nature reserves and zoos online and, if you
                 wish, donate to the needs of the animal you like
               </p>
-              <button class="btn btn__filled" onclick="window.location='pages/zoos/panda.html'">
+              <button className="btn btn__filled" onClick={() => window.location='./pages/zoos/panda.html'}>
                 Try now</button>
             </div>
           </div>
