6b5648ea31d51cacf02424ea6460fd232066377c
4ebula <4ebula@gmail.com>
2021-06-22 14:39:39 +0300

    feat: add Button component

---
+++ src/components/Button/button.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { paths } from './paths';
+
+require('./button.scss');
+
+export class Button extends React.Component {
+  constructor(props) {
+    super();
+    this.link = props.link;
+  }
+
+  goTo() {
+    window.location = this.link;
+  }
+
+  render() {
+    return (
+      <button className="btn" onClick={() => this.goTo()}>
+        <span>{this.props.text}</span>
+        <svg width="22" height="13" viewBox="0 0 22 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d={paths[this.props.svgPath]} fill="#3888FF" />
+        </svg>
+      </button>
+    );
+  }
+}
