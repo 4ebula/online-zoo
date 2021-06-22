b342a5abf1db40a2c2061e75be030efd7e22ed48
4ebula <4ebula@gmail.com>
2021-06-22 14:44:19 +0300

    feat: add card descriptions

--- src/components/Card/descriptions.js
+++ src/components/Card/descriptions.js
@@ -1,3 +1,6 @@
 export const descriptions = {
   eagle: 'The broadcast is from an island near Los Angeles. Watch their real life.',
+  gorilla: 'The broadcast comes from the Democratic Republic of the Congo in a forest area. Watch their life and their life together',
+  alligator: 'The broadcast is from Florida. See their real life',
+  panda: 'The broadcast comes from the mountainous regions of central China: Sichuan and Tibet. See their real life together.'
 }
