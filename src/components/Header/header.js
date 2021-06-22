62927702d83ff9578b93cbf4b8f3d4d3a09eda6c
4ebula <4ebula@gmail.com>
2021-06-22 12:35:38 +0300

    fix: resolve errors in header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 
 require('./header.scss');
 
 export class Header extends React.Component {
   render() {
     return (
-      <header class="header">
-        <div class="wrapper">
-          <div class="header-main">
-            <div class="logo-container">
-              <div class="logo">
+      <header className="header">
+        <div className="wrapper">
+          <div className="header-main">
+            <div className="logo-container">
+              <div className="logo">
                 <a href="#about"></a>
-                <img src="assets/images/logo.svg" alt="logo"/>
+                <img src={require("../../assets/images/logo.svg")} alt="logo"/>
               </div>
             </div>
             <nav>
               <ul>
-                <li class="active"><a href="">About</a></li>
+                <li className="active"><a href="">About</a></li>
                 <li><a href="pages/map/map.html">Map</a></li>
                 <li><a href="pages/zoos/panda.html">Zoos</a></li>
                 <li><a href="pages/404/404.html">Contact us</a></li>
@@ -25,12 +24,12 @@ export class Header extends React.Component {
                     Design</a></li>
               </ul>
             </nav>
-            <div class="login">
-              <button class="btn btn-login">
-                <object data="assets/images/icon-login.svg" type="image/svg+xml"></object>
+            <div className="login">
+              <button className="btn btn-login">
+                <object data={require("../../assets/images/icon-login.svg")} type="image/svg+xml"></object>
               </button>
             </div>
-            <div class="burger"><img src="assets/images/icons/icon-burger.svg" alt=""/></div>
+            <div className="burger"><img src={require("../../assets/images/icons/icon-burger.svg")} alt=""/></div>
           </div>
         </div>
       </header>
