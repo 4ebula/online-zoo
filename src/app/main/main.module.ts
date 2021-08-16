d8b5649f9c128c5009fd7a95b7e931c5f55d9d2a
4ebula <4ebula@gmail.com>
2021-08-16 15:05:50 +0300

    refactor: separate footer

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { AsideComponent } from './components/aside/aside.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [HeaderComponent, AsideComponent],
+  declarations: [HeaderComponent, AsideComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, AsideComponent],
+  exports: [HeaderComponent, AsideComponent, FooterComponent],
 })
 export class MainModule {}
