a0b2bb6bc1b48fa873517e6d3ff91505ba675d59
4ebula <4ebula@gmail.com>
2021-08-16 15:25:49 +0300

    refactor: separate famous-pets

---
+++ src/app/main/components/famous-pets/famous-pets.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-famous-pets',
+  templateUrl: './famous-pets.component.html',
+  styleUrls: ['./famous-pets.component.scss'],
+})
+export class FamousPetsComponent {}
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,10 +5,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
 import { HowToComponent } from './components/how-to/how-to.component';
+import { FamousPetsComponent } from './components/famous-pets/famous-pets.component';
 
 @NgModule({
-  declarations: [HeaderComponent, AsideComponent, FooterComponent, AboutComponent, HowToComponent],
+  declarations: [HeaderComponent, AsideComponent, FooterComponent, AboutComponent, HowToComponent, FamousPetsComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, AsideComponent, FooterComponent, AboutComponent, HowToComponent],
+  exports: [HeaderComponent, AsideComponent, FooterComponent, AboutComponent, HowToComponent, FamousPetsComponent],
 })
 export class MainModule {}
