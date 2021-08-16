d7008f13f87e548d73fb82988f187f37402c0fca
4ebula <4ebula@gmail.com>
2021-08-16 14:43:34 +0300

    refactor: separate sidebar

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AsideComponent } from './components/aside/aside.component';
 import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [HeaderComponent, AsideComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent],
+  exports: [HeaderComponent, AsideComponent],
 })
 export class MainModule {}
