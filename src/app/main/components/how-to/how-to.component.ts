70992c08e1c5b1ccfbe1565db299c85d4a3b68ff
4ebula <4ebula@gmail.com>
2021-08-16 15:21:02 +0300

    refactor: separate how-to

--- src/app/main/components/how-to/how-to.component.ts
+++ src/app/main/components/how-to/how-to.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-how-to',
   templateUrl: './how-to.component.html',
-  styleUrls: ['./how-to.component.scss']
+  styleUrls: ['./how-to.component.scss'],
 })
-export class HowToComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class HowToComponent {}
