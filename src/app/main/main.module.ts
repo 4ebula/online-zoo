80ed1cf66541b08d3dc19ae7996cfb75e0e0bfc8
4ebula <4ebula@gmail.com>
2021-08-16 15:13:24 +0300

    refactor: separate about

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AsideComponent } from './components/aside/aside.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { HowToComponent } from './components/how-to/how-to.component';
 
 @NgModule({
-  declarations: [HeaderComponent, AsideComponent, FooterComponent],
+  declarations: [HeaderComponent, AsideComponent, FooterComponent, AboutComponent, HowToComponent],
   imports: [CommonModule],
-  exports: [HeaderComponent, AsideComponent, FooterComponent],
+  exports: [HeaderComponent, AsideComponent, FooterComponent, AboutComponent, HowToComponent],
 })
 export class MainModule {}
